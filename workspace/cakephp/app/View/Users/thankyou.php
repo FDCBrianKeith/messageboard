6b08fb88366ec86c3b1b8717953a855f3ef80356
FDCBrianKeith <fdc.briankeith@gmail.com>
2024-07-29 09:51:01 +0800

    added thankyou page

---
+++ workspace/cakephp/app/View/Users/thankyou.php
@@ -0,0 +1,8 @@
+<div class="d-flex justify-content-center align-items-center h-100">
+    <div class="text-center">
+        <h1>Thank you for registering</h1>
+        <button class="nav-item btn">
+            Back to homepage
+        </button>
+    </div>
+</div>
